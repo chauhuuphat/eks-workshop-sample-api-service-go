c417f2d432b410fb6641ec25c0e4ee85c22a8380
chauhuuphat <71479884+chauhuuphat@users.noreply.github.com>
2021-09-26 18:06:22 +0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Chau Huu Phat"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
